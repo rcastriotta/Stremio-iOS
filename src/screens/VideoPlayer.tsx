db1847fbaafdf77bf2993eaeb24c4c7889708024
rcastriotta <63270523+rcastriotta@users.noreply.github.com>
2024-04-15 12:41:14 -0400

    improved videoPlayer UI

--- src/screens/VideoPlayer.tsx
+++ src/screens/VideoPlayer.tsx
@@ -1,13 +1,12 @@
 import { VLCPlayer } from 'react-native-vlc-media-player';
-import { View, TouchableOpacity, Text, SafeAreaView, Animated, Alert } from 'react-native';
-import { BlurView } from 'expo-blur';
+import { View, TouchableOpacity, Text, SafeAreaView, Alert, Animated } from 'react-native';
 import Ionicons from '@expo/vector-icons/Ionicons';
 import { scale } from 'react-native-size-matters';
-import colors from '../theme/colors';
 import { Slider } from '@miblanchard/react-native-slider';
 import { useEffect, useMemo, useRef, useState } from 'react';
 import useStremio from '../hooks/useStremio';
-
+import { GestureDetector, Gesture } from 'react-native-gesture-handler';
+import Animated2, { useSharedValue, useAnimatedStyle } from 'react-native-reanimated';
 interface IVideoProgress {
   currentTime: number;
   duration: number;
@@ -29,8 +28,10 @@ const VideoPlayer = ({ navigation, route }: any) => {
   const lastServerUpdate = useRef<IVideoProgress | null>();
   const creationTime = useRef(existingData._ctime || new Date().toISOString());
   const [textTrack, setTextTrack] = useState<any>(undefined);
-  const [videoScale, setVideoScale] = useState<number>(1);
   const showErrorAlert = useRef(true);
+  const savedScale = useSharedValue(1);
+  const videoScale = useSharedValue(1);
+  const preventControlsToggle = useRef<boolean>(false);
 
   useEffect(() => {
     if (currentVideoPosition > 0) {
@@ -96,7 +97,7 @@ const VideoPlayer = ({ navigation, route }: any) => {
 
   const [formattedCurrentTime, currentTimeContainerWidth] = useMemo(() => {
     const time = videoProgress ? formatTime(videoProgress.currentTime) : '--:--';
-    const width = time.replaceAll(':', '').length * 9;
+    const width = time.replaceAll(':', '').length * 10;
     return [time, width];
   }, [videoProgress]);
 
@@ -104,11 +105,12 @@ const VideoPlayer = ({ navigation, route }: any) => {
     const time = videoProgress
       ? `-${formatTime(videoProgress.duration - videoProgress.currentTime)}`
       : '--:--';
-    const width = time.replaceAll(':', '').length * 9;
+    const width = time.replaceAll(':', '').length * 10;
     return [time, width];
   }, [videoProgress]);
 
   const toggleControls = () => {
+    if (preventControlsToggle.current) return;
     if (showControls) {
       fadeOut();
     } else {
@@ -144,7 +146,7 @@ const VideoPlayer = ({ navigation, route }: any) => {
     }
   };
 
-  const onLoad = () => {
+  const onLoad = (e: any) => {
     // fixes weird bug where subtitles show
     setTextTrack(-1);
   };
@@ -166,137 +168,111 @@ const VideoPlayer = ({ navigation, route }: any) => {
     };
   }, []);
 
+  const pinchGesture = Gesture.Pinch()
+    .onUpdate(e => {
+      preventControlsToggle.current = true;
+      videoScale.value = savedScale.value * e.scale;
+    })
+    .onEnd(() => {
+      preventControlsToggle.current = false;
+      savedScale.value = videoScale.value;
+    });
+
+  const animatedStyle = useAnimatedStyle(() => ({
+    transform: [{ scale: videoScale.value }],
+    flex: 1,
+  }));
+
   return (
     <TouchableOpacity className="flex-1 bg-black" activeOpacity={1} onPress={toggleControls}>
-      <VLCPlayer
-        ref={videoPlayerRef}
-        style={{ flex: 1, backgroundColor: 'black', transform: [{ scale: videoScale }] }}
-        source={{
-          uri: url,
-        }}
-        repeat={false}
-        playInBackground={true}
-        autoAspectRatio={true}
-        onError={() => onVideoFailed()}
-        onProgress={!disableSliderUpdates ? handleVideoProgressUpdate : undefined}
-        paused={paused}
-        textTrack={textTrack}
-        muted={false}
-        onLoad={onLoad}
-      />
+      <Animated2.View style={[animatedStyle]}>
+        <VLCPlayer
+          ref={videoPlayerRef}
+          style={{ flex: 1, backgroundColor: 'black' }}
+          source={{
+            uri: url,
+          }}
+          repeat={false}
+          playInBackground={true}
+          autoAspectRatio={true}
+          onError={() => onVideoFailed()}
+          onProgress={!disableSliderUpdates ? handleVideoProgressUpdate : undefined}
+          paused={paused}
+          textTrack={textTrack}
+          muted={false}
+          onLoad={onLoad}
+        />
+      </Animated2.View>
 
-      <SafeAreaView className="z-[90] absolute w-full h-full">
-        <View className="flex-1 justify-between p-[5vw] pb-[10px]">
-          <View className="w-full justify-between flex-row">
-            <Animated.View
-              className="w-[11vw] aspect-[1]"
-              style={{
-                opacity: fadeAnim,
-                display: blockTouchEvents ? 'none' : 'flex',
-              }}>
-              <TouchableOpacity
-                onPress={() => navigation.goBack()}
-                activeOpacity={0.5}
-                className="flex-1">
-                <BlurView
-                  className="w-full h-full rounded-[10px] overflow-hidden items-center justify-center "
-                  tint="light">
-                  <Ionicons name="close" size={scale(20)} color={colors.mainText} />
-                </BlurView>
-              </TouchableOpacity>
-            </Animated.View>
-            <View className="space-x-2 flex-row">
-              <Animated.View
-                className="w-[11vw] aspect-[1]"
-                style={{
-                  opacity: fadeAnim,
-                  display: blockTouchEvents ? 'none' : 'flex',
-                }}>
-                <TouchableOpacity
-                  onPress={() => setVideoScale(p => p - 0.05)}
-                  activeOpacity={0.5}
-                  className="flex-1">
-                  <BlurView
-                    className="w-full h-full rounded-[10px] overflow-hidden items-center justify-center "
-                    tint="light">
-                    <Ionicons name="remove" size={scale(18)} color={colors.mainText} />
-                  </BlurView>
+      <GestureDetector gesture={pinchGesture}>
+        <Animated.View
+          className={'z-[90] absolute w-full h-full'}
+          style={{
+            opacity: fadeAnim,
+            backgroundColor: 'rgba(0,0,0,0.4)',
+          }}>
+          <SafeAreaView className="flex-1" style={{ display: blockTouchEvents ? 'none' : 'flex' }}>
+            <View className="flex-1 justify-between p-[5vw] pb-[5px] items-center">
+              <View className="w-full justify-between flex-row">
+                <TouchableOpacity onPress={() => navigation.goBack()} activeOpacity={0.5}>
+                  <Ionicons name="close" size={scale(30)} color={'rgba(255,255,255,0.7)'} />
                 </TouchableOpacity>
-              </Animated.View>
-              <Animated.View
-                className="w-[11vw] aspect-[1]"
-                style={{
-                  opacity: fadeAnim,
-                  display: blockTouchEvents ? 'none' : 'flex',
-                }}>
-                <TouchableOpacity
-                  onPress={() => setVideoScale(p => p + 0.05)}
-                  activeOpacity={0.5}
-                  className="flex-1">
-                  <BlurView
-                    className="w-full h-full rounded-[10px] overflow-hidden items-center justify-center "
-                    tint="light">
-                    <Ionicons name="add" size={scale(18)} color={colors.mainText} />
-                  </BlurView>
-                </TouchableOpacity>
-              </Animated.View>
-            </View>
-          </View>
+              </View>
 
-          <Animated.View
-            className="w-[100%] h-[45px] rounded-[15px] overflow-hidden"
-            style={{
-              opacity: fadeAnim,
-              display: blockTouchEvents ? 'none' : 'flex',
-            }}>
-            <BlurView className="flex-1 px-[15px] flex-row items-center space-x-4" tint="light">
-              <View className="space-x-3 flex-row items-center">
+              <View
+                className="w-full justify-evenly flex-row items-center absolute top-0 bottom-0"
+                pointerEvents="box-none">
                 <TouchableOpacity onPress={skipBackward} activeOpacity={0.5}>
-                  <Ionicons name={'play-back'} size={scale(18)} color={'rgba(255,255,255,0.7)'} />
+                  <Ionicons name={'play-back'} size={scale(30)} color={'rgba(255,255,255,0.7)'} />
                 </TouchableOpacity>
-                <TouchableOpacity onPress={() => setPaused(p => !p)} activeOpacity={0.5}>
-                  <Ionicons
-                    name={paused ? 'play' : 'pause'}
-                    size={scale(20)}
-                    color={'rgba(255,255,255,0.7)'}
-                  />
-                </TouchableOpacity>
+                <View>
+                  <TouchableOpacity onPress={() => setPaused(p => !p)} activeOpacity={0.5}>
+                    <Ionicons
+                      name={paused ? 'play' : 'pause'}
+                      size={scale(50)}
+                      color={'rgba(255,255,255,0.7)'}
+                    />
+                  </TouchableOpacity>
+                </View>
                 <TouchableOpacity onPress={skipForward} activeOpacity={0.5}>
                   <Ionicons
                     name={'play-forward'}
-                    size={scale(18)}
+                    size={scale(30)}
                     color={'rgba(255,255,255,0.7)'}
                   />
                 </TouchableOpacity>
               </View>
-              <View className="items-center flex-row space-x-3 flex-1">
-                <View style={{ minWidth: currentTimeContainerWidth }} className="items-center">
-                  <Text style={{ color: 'rgba(255,255,255,0.6)', fontSize: 12 }}>
-                    {formattedCurrentTime}
-                  </Text>
-                </View>
 
-                <Slider
-                  containerStyle={{ flex: 1, marginLeft: 15 }}
-                  minimumValue={0}
-                  maximumValue={1}
-                  onSlidingStart={() => setDisableSliderUpdates(true)}
-                  onSlidingComplete={(v: any) => onSeekComplete(v[0])}
-                  value={sliderPosition}
-                  minimumTrackStyle={{ backgroundColor: 'rgba(255,255,255,0.7)' }}
-                  maximumTrackStyle={{ backgroundColor: 'rgba(0,0,0,0.4)' }}
-                  thumbStyle={{ backgroundColor: 'rgba(255,255,255,1)', height: 10, width: 10 }}
-                />
-                <View style={{ minWidth: remainingTimeContainerWidth }}>
-                  <Text style={{ color: 'rgba(255,255,255,0.6)', fontSize: 12 }}>
-                    {formattedRemainingTime}
-                  </Text>
+              <View className="w-[100%] h-[45px] rounded-[15px] overflow-hidden">
+                <View className="items-center flex-row space-x-3 flex-1">
+                  <View style={{ minWidth: currentTimeContainerWidth }} className="items-center">
+                    <Text style={{ color: 'rgba(255,255,255,0.6)', fontSize: 13 }}>
+                      {formattedCurrentTime}
+                    </Text>
+                  </View>
+
+                  <Slider
+                    containerStyle={{ flex: 1, marginLeft: 15 }}
+                    minimumValue={0}
+                    maximumValue={1}
+                    onSlidingStart={() => setDisableSliderUpdates(true)}
+                    onSlidingComplete={(v: any) => onSeekComplete(v[0])}
+                    value={sliderPosition}
+                    minimumTrackStyle={{ backgroundColor: 'rgba(255,255,255,0.7)' }}
+                    maximumTrackStyle={{ backgroundColor: 'rgba(255,255,255,0.2)' }}
+                    thumbStyle={{ backgroundColor: 'rgba(255,255,255,1)', height: 12, width: 12 }}
+                  />
+                  <View style={{ minWidth: remainingTimeContainerWidth }}>
+                    <Text style={{ color: 'rgba(255,255,255,0.6)', fontSize: 13 }}>
+                      {formattedRemainingTime}
+                    </Text>
+                  </View>
                 </View>
               </View>
-            </BlurView>
-          </Animated.View>
-        </View>
-      </SafeAreaView>
+            </View>
+          </SafeAreaView>
+        </Animated.View>
+      </GestureDetector>
     </TouchableOpacity>
   );
 };
